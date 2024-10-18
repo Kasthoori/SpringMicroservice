499a04083669320f20a879f46d27ff5c887fe613
Kasthoori <ap.kasth@gmail.com>
2024-10-18 22:27:01 +1300

    product creation completed

--- product-service/src/main/java/com/dev/spring/controller/ProductController.java
+++ product-service/src/main/java/com/dev/spring/controller/ProductController.java
@@ -8,14 +8,22 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dev.spring.dto.ProductRequest;
+import com.dev.spring.service.ProductService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
+	
+	private final ProductService productService;
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public void createProduct(@RequestBody ProductRequest productRequest ) {
+		
+		productService.createProduct(productRequest);
 		
 	}
 
--- product-service/src/main/java/com/dev/spring/service/ProductService.java
+++ product-service/src/main/java/com/dev/spring/service/ProductService.java
@@ -4,12 +4,20 @@ import org.springframework.stereotype.Service;
 
 import com.dev.spring.dto.ProductRequest;
 import com.dev.spring.model.Product;
+import com.dev.spring.repository.ProductRepository;
 
 import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Builder
+@RequiredArgsConstructor
+@Slf4j
 public class ProductService {
+	
+	// To save products in database it need product repository class and inject it
+	private final ProductRepository productRepository;
 	
 	public void createProduct(ProductRequest productRequest) {
 		
@@ -18,6 +26,9 @@ public class ProductService {
 				.description(productRequest.getDescription())
 				.price(productRequest.getPrice())
 				.build();
+		
+		productRepository.save(product);
+		log.info("Product Successfully Saved");
 	}
 
 }
