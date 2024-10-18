0c889269b76cb0da68c6d290e0c2783c4ea49811
Kasthoori <ap.kasth@gmail.com>
2024-10-18 13:35:11 +1300

    remove project

--- product-service/src/main/java/com/programmingmicro/productservice/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.programmingmicro.productservice.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.programmingmicro.productservice.model.Product;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-
-}
