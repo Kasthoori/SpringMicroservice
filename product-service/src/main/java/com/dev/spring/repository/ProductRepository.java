6bd543de7f8dd0c1596741cc678b8922fba061da
Kasthoori <ap.kasth@gmail.com>
2024-11-14 20:08:51 +1300

    initial commit

--- product-service/src/main/java/com/dev/spring/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dev.spring.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.dev.spring.model.Product;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-
-}
